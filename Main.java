6c56a636c70a793aa170d240bac12a8742fee7b5
sufster <127886368+sufster@users.noreply.github.com>
2026-02-20 00:21:55 +0200

    data types

---
+++ Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Main {
+    static void main(String[] args){
+        //Exercise part 1
+        int num1 = 10;
+        int num2 = 20;
+        System.out.println(num1 + " " + num2);
+
+        //Exercise part 2
+        String hello = "hello ";
+        String fren = "my friend";
+        System.out.println(hello + fren);
+
+        //Exercise part 3
+        Double dub1 = 1.5;
+        Double dub2 = 2.5;
+        System.out.println(dub1.toString() + " " + dub2.toString());
+
+        //Exercise part 4
+        char varChar = 'Y';
+        char varChar2 = 'X';
+        System.out.print(varChar);
+        System.out.println(varChar2);
+
+        //Exercise part 5
+        Boolean yes = true;
+        Boolean no = false;
+        System.out.println(yes);
+        System.out.println(no);
+    }
+}
