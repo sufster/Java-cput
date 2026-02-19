8eb7f5313c64236bfa00c3b18b1babc3220db68a
sufster <127886368+sufster@users.noreply.github.com>
2026-02-19 14:15:27 +0200

    Add files via upload

---
+++ FixMe.java
@@ -0,0 +1,8 @@
+public class FixMe {
+    public static void main(String[] args){
+        System.out.println("I am broken. Fix me");
+        System.out.println("Now what is wrong with me now?");
+        System.out.println("Hello, please help me.");
+        System.out.println("Finally I think I am ok");
+    }
+}
